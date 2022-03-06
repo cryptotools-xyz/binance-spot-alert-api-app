7042872fc93df5b9e554df7b8c00082c92c59e5f
Souhail M <souhail.merroun1996@gmail.com>
2022-03-06 14:48:50 +0100

    Setup a new command

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Notifications\BTC_BUSDPriceReached; 
-use Illuminate\Support\Facades\Notification;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,8 +15,4 @@ use Illuminate\Support\Facades\Notification;
 
 Route::get('/', function () {
     return view('welcome');
-});
-
-Route::get('/BTC_BUSD', function () {
-    Notification::route('slack', 'https://hooks.slack.com/services/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')->notify(new BTC_BUSDPriceReached(30000));
 });
